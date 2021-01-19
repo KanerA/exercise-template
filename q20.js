10f7c4f47400e51abe961b672aae582a26a86a0f
KanerA <kaner.assaf@gmail.com>
2021-01-19 20:41:33 +0200

    answer q20

--- q20.js
+++ q20.js
@@ -2,10 +2,28 @@ function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
-
-/**/
+    let arr= JSON.parse(input);
+    let newArr=[];
+    for(let i=0; i<arr.length; i++){
+        let counter=0;
+        let pend=arr[i];
+        for(let j=0; j<arr.length; j++){
+            if(arr[i]===arr[j]){
+                counter++;
+            }
+        }
+        if(counter>1){
+            let isTrue= false;
+            for(let k=0; k < newArr.length; k++){
+                if(pend === newArr[k]){
+                    isTrue= true;
+                }
+            }
+            if(!isTrue){
+                newArr.push(arr[i]);
+        }
+        }
+    }
+    out= newArr;
     output.innerText = out;
 } 

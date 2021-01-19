bbe2889784d1f5467d8d08408e13d301800b7a77
KanerA <kaner.assaf@gmail.com>
2021-01-19 23:45:43 +0200

    answer q7

--- q7.js
+++ q7.js
@@ -2,10 +2,27 @@ function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
-
-/**/
+    let arr = JSON.parse(input);
+    let sortedArr=[];
+    OgLen=arr.length;
+    while(arr.length!==0){
+        let num=arrBig(arr);  
+        sortedArr.unshift(arr[num]);
+        arr.splice(num,1);
+    }
+    out = sortedArr;
     output.innerText = out;
 } 
+
+function arrBig(arr) {
+    let check=arr[0];
+    let index=0;
+    for(let i=0; i<arr.length; i++){
+            if(check< arr[i]){
+                check=arr[i];
+                index= i;
+            }
+           
+    }
+    return index;
+}

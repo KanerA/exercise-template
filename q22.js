3e41ffced5195b8cbbfc0b52854cebff86f4b3b5
KanerA <kaner.assaf@gmail.com>
2021-01-20 00:35:23 +0200

    answer q22

--- q22.js
+++ q22.js
@@ -1,11 +1,19 @@
 function run() {
     const input = document.getElementById("input").value;
+    const input2 = document.getElementById("input2").value;
     const output = document.getElementById("output");
+    let array1= JSON.parse(input);
+    let array2= JSON.parse(input2);
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
-
-/**/
+    let arrNew=array1.concat(array2);
+    arrNew.sort(function(a, b){return a-b});
+   let counter=0;
+   for(let i=0; i< arrNew.length; i++){
+       if(arrNew[i]=== arrNew[i+1]){
+           arrNew.splice(i,1);
+           i--;
+       }
+   }
+    out= arrNew;
     output.innerText = out;
 } 

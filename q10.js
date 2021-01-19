8839161a7ba66d58132c4830ac1064e9d716c035
KanerA <kaner.assaf@gmail.com>
2021-01-19 18:19:58 +0200

    answer q10

--- q10.js
+++ q10.js
@@ -2,10 +2,18 @@ function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
+    let text="";
+    let arr = JSON.parse(input);
+    for(let i=0; i<arr.length; i++){
+        text += "row"+ i;
+        for(let j=0; j<arr[i].length; j++){
+            text += '\n';
+            text += arr[i][j];
+        }
+        text += "\n";
+    }
+    out = text;
 
-/**/
+
     output.innerText = out;
 } 

3ac3ecbd8733ed58e2b7417a482523b67242b6e8
KanerA <kaner.assaf@gmail.com>
2021-01-19 19:44:32 +0200

    answer q18

--- q18.js
+++ q18.js
@@ -1,11 +1,18 @@
 function run() {
     const input = document.getElementById("input").value;
+    const input2 = document.getElementById("input2").value;
     const output = document.getElementById("output");
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
+    let text=0;
+    let arr=JSON.parse(input);
+    const index=Number(input2);
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]=== index){
+            text = i;
+            break;
+        }
 
-/**/
+    }
+    out = text;
     output.innerText = out;
 } 
